31279c7c8252d69f06d60e8622543122cb945638
RedPenguin101 <allaboutthatmace1789@gmail.com>
2021-12-03 12:47:07 +0000

    day3 tweak

--- clojure/src/aoc2021/day03.clj
+++ clojure/src/aoc2021/day03.clj
@@ -1,15 +1,11 @@
 (ns aoc2021.day03
   (:require [clojure.string :as str]))
 
-(def example (str/split-lines "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010"))
 (def input (str/split-lines (slurp "resources/day03input.txt")))
 (defn pivot [xs] (apply map vector xs))
 (defn least-most-common [xs] (->> xs frequencies (sort-by val) (map first)))
 
-(->> input
-     pivot
-     (map least-most-common)
-     pivot
+(->> (pivot (map least-most-common (pivot input)))
      (map #(Long/parseLong (apply str %) 2))
      (apply *))
 ;; => 852500
