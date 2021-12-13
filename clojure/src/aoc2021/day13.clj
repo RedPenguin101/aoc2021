ad20de6f45354b05253b871002219a5884ca0f53
RedPenguin101 <allaboutthatmace1789@gmail.com>
2021-12-13 14:27:14 +0000

    day13 refactor

--- clojure/src/aoc2021/day13.clj
+++ clojure/src/aoc2021/day13.clj
@@ -3,37 +3,25 @@
             [clojure.string :as str]
             [aoc2021.utils :as u]))
 
-(defn initial-canvas-size [folds]
-  [(inc (* (second (first (drop-while #(not= 0 (first %)) folds))) 2))
-   (inc (* (second (first (drop-while #(not= 1 (first %)) folds))) 2))])
-
 (defn parse [input-str]
-  (let [[coords instr] (str/split input-str #"\n\n")
-        parsed-instr  (map vector (map #(if (= "x" %) 0 1) (re-seq #"[xy]" instr)) (u/extract-ints instr))]
-    [{:dots (set (map vec (partition 2 (u/extract-ints coords))))
-      :size (initial-canvas-size parsed-instr)}
-     parsed-instr]))
+  (let [[coords instr] (str/split input-str #"\n\n")]
+    [(set (map vec (partition 2 (u/extract-ints coords))))
+     (map vector (map #(if (= "x" %) 0 1) (re-seq #"[xy]" instr)) (u/extract-ints instr))]))
 
 (def input (parse (slurp "resources/day13input.txt")))
 
-(defn new-coord [size axis fold-line coord]
+(defn new-coord [axis fold-line coord]
   (when (> (get coord axis) fold-line)
-    (update coord axis #(- (get size axis) % 1))))
+    (update coord axis #(- (* 2 fold-line) %))))
 
-(defn new-dots [dots size axis fold-line]
+(defn fold [dots [axis fold-line]]
   (set/union (set (remove #(>= (get % axis) fold-line) dots))
-             (set (keep #(new-coord size axis fold-line %) dots))))
+             (set (keep #(new-coord axis fold-line %) dots))))
 
-(defn new-canvas-size [current axis] (update current axis #(/ (dec %) 2)))
-
-(defn fold [canvas [axis fold-line]]
-  (-> (update canvas :dots new-dots (:size canvas) axis fold-line)
-      (update        :size new-canvas-size axis)))
-
-(count (:dots (fold (first input) (first (second input)))))
+(count (fold (first input) (first (second input))))
 ; 737
 
-(println (u/print-coord-set (:dots (apply reduce fold input))))
+(println (u/print-coord-set (apply reduce fold input)))
 ; "####.#..#...##.#..#..##..####.#..#.###..
 ;  ...#.#..#....#.#..#.#..#.#....#..#.#..#.
 ;  ..#..#..#....#.#..#.#..#.###..####.#..#.
