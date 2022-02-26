38920483750bb1e6321b61ce508f69c43f4c5c93
RedPenguin101 <allaboutthatmace1789@gmail.com>
2022-02-26 10:59:03 +0000

    zig day3 part 1

--- zig/src/day03.zig
+++ zig/src/day03.zig
@@ -3,4 +3,32 @@ const std = @import("std");
 const stdin = std.io.getStdIn();
 const stdout = std.io.getStdOut();
 
-pub fn main() !void {}
+pub fn main() !void {
+    var line_buff: [13]u8 = undefined;
+    var num_count: usize = 0;
+    var ones: [12]usize = .{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+    while (try stdin.reader().readUntilDelimiterOrEof(&line_buff, '\n')) |_| {
+        for (line_buff) |char, idx| {
+            if (char == '1') {
+                ones[idx] += 1;
+            }
+        }
+        num_count += 1;
+    }
+
+    const cmp = num_count / 2;
+    var most_bin: usize = 0;
+    var least_bin: usize = 0;
+
+    for (ones) |num_ones| {
+        add_right(&most_bin, if (num_ones > cmp) 1 else 0);
+        add_right(&least_bin, if (num_ones < cmp) 1 else 0);
+    }
+
+    try stdout.writer().print("Day 3 Part 1: {d}\n", .{most_bin * least_bin});
+}
+
+fn add_right(num: *usize, new_bit: u8) void {
+    num.* = (num.* << 1) | new_bit;
+}
