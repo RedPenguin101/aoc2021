5983dd8ad8ab808f2b26bfbf46ccb6d0bee6ca72
RedPenguin101 <allaboutthatmace1789@gmail.com>
2022-02-26 12:10:51 +0000

    refactor with most/least

--- zig/src/day03.zig
+++ zig/src/day03.zig
@@ -6,33 +6,56 @@ const stdout = std.io.getStdOut();
 pub fn main() !void {
     var line_buff: [13]u8 = undefined;
     var num_count: usize = 0;
-    var ones: [12]usize = .{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    var input: [1000][13]u8 = undefined;
 
-    while (try stdin.reader().readUntilDelimiterOrEof(&line_buff, '\n')) |num_str| {
-        count_ones(&ones, num_str);
+    while (try stdin.reader().readUntilDelimiterOrEof(&line_buff, '\n')) |_| {
+        input[num_count] = line_buff;
         num_count += 1;
     }
 
-    const cmp = num_count / 2;
-    var most_bin: usize = 0;
-    var least_bin: usize = 0;
-
-    for (ones) |num_ones| {
-        add_right(&most_bin, if (num_ones > cmp) 1 else 0);
-        add_right(&least_bin, if (num_ones < cmp) 1 else 0);
-    }
-
-    try stdout.writer().print("Day 3 Part 1: {d}\n", .{most_bin * least_bin});
+    try stdout.writer().print("Day 3 Part 1: {d}\n", .{most_common(input) * least_common(input)});
 }
 
 fn add_right(num: *usize, new_bit: u8) void {
     num.* = (num.* << 1) | new_bit;
 }
 
-fn count_ones(ones: *[12]usize, num_str: []u8) void {
+fn count_ones(ones: *[12]usize, num_str: [13]u8) void {
     for (num_str) |char, idx| {
         if (char == '1') {
             ones.*[idx] += 1;
         }
     }
+}
+
+fn most_common(nums: [1000][13]u8) usize {
+    var ones: [12]usize = .{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    var cmp: usize = 0;
+    for (nums) |num_str| {
+        count_ones(&ones, num_str);
+        cmp += 1;
+    }
+    cmp = cmp / 2;
+    var out: usize = 0;
+    for (ones) |num_ones| {
+        add_right(&out, if (num_ones > cmp) 1 else 0);
+    }
+    return out;
+}
+
+fn least_common(nums: [1000][13]u8) usize {
+    var ones: [12]usize = .{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    var cmp: usize = 0;
+
+    for (nums) |num_str| {
+        count_ones(&ones, num_str);
+        cmp += 1;
+    }
+
+    cmp = cmp / 2;
+    var out: usize = 0;
+    for (ones) |num_ones| {
+        add_right(&out, if (num_ones < cmp) 1 else 0);
+    }
+    return out;
 }
